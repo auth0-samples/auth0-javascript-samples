2d98d0db23e3a7456b094f07c4aac6ed1095412e
Benjamin Lizardo <benjamin.lizardo@techaid.co>
2019-06-11 09:34:11 -0400

    Removed JSON stringify

--- 01-Login/app.js
+++ 01-Login/app.js
@@ -1,124 +1,129 @@
-window.addEventListener('load', function() {
-  var idToken;
-  var accessToken;
-  var expiresAt;
+window.addEventListener("load", function() {
+    var idToken;
+    var accessToken;
+    var expiresAt;
 
-  var content = document.querySelector('.content');
-  var loadingSpinner = document.getElementById('loading');
-  content.style.display = 'block';
-  loadingSpinner.style.display = 'none';
+    var content = document.querySelector(".content");
+    var loadingSpinner = document.getElementById("loading");
+    content.style.display = "block";
+    loadingSpinner.style.display = "none";
 
-  var webAuth = new auth0.WebAuth({
-    domain: AUTH0_DOMAIN,
-    clientID: AUTH0_CLIENT_ID,
-    redirectUri: AUTH0_CALLBACK_URL,
-    responseType: 'token id_token',
-    scope: 'openid',
-    leeway: 60
-  });
-
-  var loginStatus = document.querySelector('.container h4');
-  var loginView = document.getElementById('login-view');
-  var homeView = document.getElementById('home-view');
-
-  // buttons and event listeners
-  var homeViewBtn = document.getElementById('btn-home-view');
-  var loginBtn = document.getElementById('qsLoginBtn');
-  var logoutBtn = document.getElementById('qsLogoutBtn');
-
-  homeViewBtn.addEventListener('click', function() {
-    homeView.style.display = 'inline-block';
-    loginView.style.display = 'none';
-  });
-
-  loginBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    webAuth.authorize();
-  });
-
-  logoutBtn.addEventListener('click', logout);
-
-  function localLogin(authResult) {
-    // Set isLoggedIn flag in localStorage
-    localStorage.setItem('isLoggedIn', 'true');
-    // Set the time that the access token will expire at
-    expiresAt = JSON.stringify(
-      authResult.expiresIn * 1000 + new Date().getTime()
-    );
-    accessToken = authResult.accessToken;
-    idToken = authResult.idToken;
-  }
-
-  function renewTokens() {
-    webAuth.checkSession({}, (err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        localLogin(authResult);
-      } else if (err) {
-        alert(
-            'Could not get a new token '  + err.error + ':' + err.error_description + '.'
-        );
-        logout();
-      }
-      displayButtons();
-    });
-  }
-
-  function logout() {
-    // Remove isLoggedIn flag from localStorage
-    localStorage.removeItem('isLoggedIn');
-    // Remove tokens and expiry time
-    accessToken = '';
-    idToken = '';
-    expiresAt = 0;
-
-    webAuth.logout({
-      return_to: window.location.origin
+    var webAuth = new auth0.WebAuth({
+        domain: AUTH0_DOMAIN,
+        clientID: AUTH0_CLIENT_ID,
+        redirectUri: AUTH0_CALLBACK_URL,
+        responseType: "token id_token",
+        scope: "openid",
+        leeway: 60
     });
 
-    displayButtons();
-  }
+    var loginStatus = document.querySelector(".container h4");
+    var loginView = document.getElementById("login-view");
+    var homeView = document.getElementById("home-view");
 
-  function isAuthenticated() {
-    // Check whether the current time is past the
-    // access token's expiry time
-    var expiration = parseInt(expiresAt) || 0;
-    return localStorage.getItem('isLoggedIn') === 'true' && new Date().getTime() < expiration;
-  }
+    // buttons and event listeners
+    var homeViewBtn = document.getElementById("btn-home-view");
+    var loginBtn = document.getElementById("qsLoginBtn");
+    var logoutBtn = document.getElementById("qsLogoutBtn");
 
-  function handleAuthentication() {
-    webAuth.parseHash(function(err, authResult) {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        window.location.hash = '';
-        localLogin(authResult);
-        loginBtn.style.display = 'none';
-        homeView.style.display = 'inline-block';
-      } else if (err) {
-        homeView.style.display = 'inline-block';
-        console.log(err);
-        alert(
-          'Error: ' + err.error + '. Check the console for further details.'
-        );
-      }
-      displayButtons();
+    homeViewBtn.addEventListener("click", function() {
+        homeView.style.display = "inline-block";
+        loginView.style.display = "none";
     });
-  }
 
-  function displayButtons() {
-    if (isAuthenticated()) {
-      loginBtn.style.display = 'none';
-      logoutBtn.style.display = 'inline-block';
-      loginStatus.innerHTML = 'You are logged in!';
-    } else {
-      loginBtn.style.display = 'inline-block';
-      logoutBtn.style.display = 'none';
-      loginStatus.innerHTML =
-        'You are not logged in! Please log in to continue.';
+    loginBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+        webAuth.authorize();
+    });
+
+    logoutBtn.addEventListener("click", logout);
+
+    function localLogin(authResult) {
+        // Set isLoggedIn flag in localStorage
+        localStorage.setItem("isLoggedIn", "true");
+        // Set the time that the access token will expire at
+        expiresAt = (authResult.expiresIn * 1000 + new Date().getTime()).toString();
+        accessToken = authResult.accessToken;
+        idToken = authResult.idToken;
     }
-  }
 
-  if (localStorage.getItem('isLoggedIn') === 'true') {
-    renewTokens();
-  } else {
-    handleAuthentication();
-  }
+    function renewTokens() {
+        webAuth.checkSession({}, (err, authResult) => {
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                localLogin(authResult);
+            } else if (err) {
+                alert(
+                    "Could not get a new token " +
+                    err.error +
+                    ":" +
+                    err.error_description +
+                    "."
+                );
+                logout();
+            }
+            displayButtons();
+        });
+    }
+
+    function logout() {
+        // Remove isLoggedIn flag from localStorage
+        localStorage.removeItem("isLoggedIn");
+        // Remove tokens and expiry time
+        accessToken = "";
+        idToken = "";
+        expiresAt = 0;
+
+        webAuth.logout({
+            return_to: window.location.origin
+        });
+
+        displayButtons();
+    }
+
+    function isAuthenticated() {
+        // Check whether the current time is past the
+        // access token's expiry time
+        var expiration = parseInt(expiresAt) || 0;
+        return (
+            localStorage.getItem("isLoggedIn") === "true" &&
+            new Date().getTime() < expiration
+        );
+    }
+
+    function handleAuthentication() {
+        webAuth.parseHash(function(err, authResult) {
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                window.location.hash = "";
+                localLogin(authResult);
+                loginBtn.style.display = "none";
+                homeView.style.display = "inline-block";
+            } else if (err) {
+                homeView.style.display = "inline-block";
+                console.log(err);
+                alert(
+                    "Error: " + err.error + ". Check the console for further details."
+                );
+            }
+            displayButtons();
+        });
+    }
+
+    function displayButtons() {
+        if (isAuthenticated()) {
+            loginBtn.style.display = "none";
+            logoutBtn.style.display = "inline-block";
+            loginStatus.innerHTML = "You are logged in!";
+        } else {
+            loginBtn.style.display = "inline-block";
+            logoutBtn.style.display = "none";
+            loginStatus.innerHTML =
+                "You are not logged in! Please log in to continue.";
+        }
+    }
+
+    if (localStorage.getItem("isLoggedIn") === "true") {
+        renewTokens();
+    } else {
+        handleAuthentication();
+    }
 });
--- 02-User-Profile/app.js
+++ 02-User-Profile/app.js
@@ -1,167 +1,172 @@
-window.addEventListener('load', function() {
-  var idToken;
-  var accessToken;
-  var expiresAt;
+window.addEventListener("load", function() {
+    var idToken;
+    var accessToken;
+    var expiresAt;
 
-  var userProfile;
-  var content = document.querySelector('.content');
-  var loadingSpinner = document.getElementById('loading');
-  content.style.display = 'block';
-  loadingSpinner.style.display = 'none';
+    var userProfile;
+    var content = document.querySelector(".content");
+    var loadingSpinner = document.getElementById("loading");
+    content.style.display = "block";
+    loadingSpinner.style.display = "none";
 
-  var webAuth = new auth0.WebAuth({
-    domain: AUTH0_DOMAIN,
-    clientID: AUTH0_CLIENT_ID,
-    redirectUri: AUTH0_CALLBACK_URL,
-    responseType: 'token id_token',
-    scope: 'openid profile',
-    leeway: 60
-  });
-
-  var loginStatus = document.querySelector('.container h4');
-  var loginView = document.getElementById('login-view');
-  var homeView = document.getElementById('home-view');
-  var profileView = document.getElementById('profile-view');
-
-  // buttons and event listeners
-  var loginBtn = document.getElementById('qsLoginBtn');
-  var logoutBtn = document.getElementById('qsLogoutBtn');
-
-  var homeViewBtn = document.getElementById('btn-home-view');
-  var profileViewBtn = document.getElementById('btn-profile-view');
-
-  homeViewBtn.addEventListener('click', function() {
-    homeView.style.display = 'inline-block';
-    profileView.style.display = 'none';
-  });
-
-  profileViewBtn.addEventListener('click', function() {
-    homeView.style.display = 'none';
-    profileView.style.display = 'inline-block';
-    getProfile();
-  });
-
-  loginBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    webAuth.authorize();
-  });
-
-  logoutBtn.addEventListener('click', logout);
-
-  function localLogin(authResult) {
-    // Set isLoggedIn flag in localStorage
-    localStorage.setItem('isLoggedIn', 'true');
-    // Set the time that the access token will expire at
-    expiresAt = JSON.stringify(
-      authResult.expiresIn * 1000 + new Date().getTime()
-    );
-    accessToken = authResult.accessToken;
-    idToken = authResult.idToken;
-  }
-
-  function renewTokens() {
-    webAuth.checkSession({}, (err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        localLogin(authResult);
-      } else if (err) {
-        alert(
-            'Could not get a new token '  + err.error + ':' + err.error_description + '.'
-        );
-        logout();
-      }
-      displayButtons();
-    });
-  }
-
-  function logout() {
-    // Remove isLoggedIn flag from localStorage
-    localStorage.removeItem('isLoggedIn');
-    // Remove tokens and expiry time
-    accessToken = '';
-    idToken = '';
-    expiresAt = 0;
-
-    webAuth.logout({
-      return_to: window.location.origin
+    var webAuth = new auth0.WebAuth({
+        domain: AUTH0_DOMAIN,
+        clientID: AUTH0_CLIENT_ID,
+        redirectUri: AUTH0_CALLBACK_URL,
+        responseType: "token id_token",
+        scope: "openid profile",
+        leeway: 60
     });
 
-    displayButtons();
-  }
+    var loginStatus = document.querySelector(".container h4");
+    var loginView = document.getElementById("login-view");
+    var homeView = document.getElementById("home-view");
+    var profileView = document.getElementById("profile-view");
 
-  function isAuthenticated() {
-    // Check whether the current time is past the
-    // access token's expiry time
-    var expiration = parseInt(expiresAt) || 0;
-    return localStorage.getItem('isLoggedIn') === 'true' && new Date().getTime() < expiration;
-  }
+    // buttons and event listeners
+    var loginBtn = document.getElementById("qsLoginBtn");
+    var logoutBtn = document.getElementById("qsLogoutBtn");
 
-  function displayButtons() {
-    var loginStatus = document.querySelector('.container h4');
-    if (isAuthenticated()) {
-      loginBtn.style.display = 'none';
-      logoutBtn.style.display = 'inline-block';
-      profileViewBtn.style.display = 'inline-block';
-      loginStatus.innerHTML =
-        'You are logged in! You can now view your profile area.';
-    } else {
-      homeView.style.display = 'inline-block';
-      loginBtn.style.display = 'inline-block';
-      logoutBtn.style.display = 'none';
-      profileViewBtn.style.display = 'none';
-      profileView.style.display = 'none';
-      loginStatus.innerHTML =
-        'You are not logged in! Please log in to continue.';
+    var homeViewBtn = document.getElementById("btn-home-view");
+    var profileViewBtn = document.getElementById("btn-profile-view");
+
+    homeViewBtn.addEventListener("click", function() {
+        homeView.style.display = "inline-block";
+        profileView.style.display = "none";
+    });
+
+    profileViewBtn.addEventListener("click", function() {
+        homeView.style.display = "none";
+        profileView.style.display = "inline-block";
+        getProfile();
+    });
+
+    loginBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+        webAuth.authorize();
+    });
+
+    logoutBtn.addEventListener("click", logout);
+
+    function localLogin(authResult) {
+        // Set isLoggedIn flag in localStorage
+        localStorage.setItem("isLoggedIn", "true");
+        // Set the time that the access token will expire at
+        expiresAt = (authResult.expiresIn * 1000 + new Date().getTime()).toString();
+        accessToken = authResult.accessToken;
+        idToken = authResult.idToken;
     }
-  }
 
-  function getProfile() {
-    if (!userProfile) {
-      if (!accessToken) {
-        console.log('Access token must exist to fetch profile');
-      }
+    function renewTokens() {
+        webAuth.checkSession({}, (err, authResult) => {
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                localLogin(authResult);
+            } else if (err) {
+                alert(
+                    "Could not get a new token " +
+                    err.error +
+                    ":" +
+                    err.error_description +
+                    "."
+                );
+                logout();
+            }
+            displayButtons();
+        });
+    }
 
-      webAuth.client.userInfo(accessToken, function(err, profile) {
-        if (profile) {
-          userProfile = profile;
-          displayProfile();
+    function logout() {
+        // Remove isLoggedIn flag from localStorage
+        localStorage.removeItem("isLoggedIn");
+        // Remove tokens and expiry time
+        accessToken = "";
+        idToken = "";
+        expiresAt = 0;
+
+        webAuth.logout({
+            return_to: window.location.origin
+        });
+
+        displayButtons();
+    }
+
+    function isAuthenticated() {
+        // Check whether the current time is past the
+        // access token's expiry time
+        var expiration = parseInt(expiresAt) || 0;
+        return (
+            localStorage.getItem("isLoggedIn") === "true" &&
+            new Date().getTime() < expiration
+        );
+    }
+
+    function displayButtons() {
+        var loginStatus = document.querySelector(".container h4");
+        if (isAuthenticated()) {
+            loginBtn.style.display = "none";
+            logoutBtn.style.display = "inline-block";
+            profileViewBtn.style.display = "inline-block";
+            loginStatus.innerHTML =
+                "You are logged in! You can now view your profile area.";
+        } else {
+            homeView.style.display = "inline-block";
+            loginBtn.style.display = "inline-block";
+            logoutBtn.style.display = "none";
+            profileViewBtn.style.display = "none";
+            profileView.style.display = "none";
+            loginStatus.innerHTML =
+                "You are not logged in! Please log in to continue.";
         }
-      });
-    } else {
-      displayProfile();
     }
-  }
 
-  function displayProfile() {
-    // display the profile
-    document.querySelector('#profile-view .nickname').innerHTML =
-      userProfile.nickname;
-    document.querySelector(
-      '#profile-view .full-profile'
-    ).innerHTML = JSON.stringify(userProfile, null, 2);
-    document.querySelector('#profile-view img').src = userProfile.picture;
-  }
+    function getProfile() {
+        if (!userProfile) {
+            if (!accessToken) {
+                console.log("Access token must exist to fetch profile");
+            }
 
-  function handleAuthentication() {
-    webAuth.parseHash(function(err, authResult) {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        window.location.hash = '';
-        localLogin(authResult);
-        loginBtn.style.display = 'none';
-        homeView.style.display = 'inline-block';
-      } else if (err) {
-        homeView.style.display = 'inline-block';
-        console.log(err);
-        alert(
-          'Error: ' + err.error + '. Check the console for further details.'
-        );
-      }
-      displayButtons();
-    });
-  }
+            webAuth.client.userInfo(accessToken, function(err, profile) {
+                if (profile) {
+                    userProfile = profile;
+                    displayProfile();
+                }
+            });
+        } else {
+            displayProfile();
+        }
+    }
 
-  if (localStorage.getItem('isLoggedIn') === 'true') {
-    renewTokens();
-  } else {
-    handleAuthentication();
-  }
+    function displayProfile() {
+        // display the profile
+        document.querySelector("#profile-view .nickname").innerHTML =
+            userProfile.nickname;
+        document.querySelector(
+            "#profile-view .full-profile"
+        ).innerHTML = JSON.stringify(userProfile, null, 2);
+        document.querySelector("#profile-view img").src = userProfile.picture;
+    }
+
+    function handleAuthentication() {
+        webAuth.parseHash(function(err, authResult) {
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                window.location.hash = "";
+                localLogin(authResult);
+                loginBtn.style.display = "none";
+                homeView.style.display = "inline-block";
+            } else if (err) {
+                homeView.style.display = "inline-block";
+                console.log(err);
+                alert(
+                    "Error: " + err.error + ". Check the console for further details."
+                );
+            }
+            displayButtons();
+        });
+    }
+
+    if (localStorage.getItem("isLoggedIn") === "true") {
+        renewTokens();
+    } else {
+        handleAuthentication();
+    }
 });
